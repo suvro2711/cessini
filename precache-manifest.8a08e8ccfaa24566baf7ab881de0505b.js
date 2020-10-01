02d34712d341530f5c0ada6d402e3f036567dc1a
Shubhro Banerjee <suvro2711@gmail.com>
2020-10-02 05:02:32 +0530

    Updates

---
+++ precache-manifest.8a08e8ccfaa24566baf7ab881de0505b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e394806c3cb0be720690859b16394107",
+    "url": "/cessini-mail/index.html"
+  },
+  {
+    "revision": "5d5bb7f0b1f35acbdfa7",
+    "url": "/cessini-mail/static/css/main.402abc36.chunk.css"
+  },
+  {
+    "revision": "f61d38f8dabac75c98f4",
+    "url": "/cessini-mail/static/js/2.011600f6.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/cessini-mail/static/js/2.011600f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d5bb7f0b1f35acbdfa7",
+    "url": "/cessini-mail/static/js/main.fbeb2553.chunk.js"
+  },
+  {
+    "revision": "30f427b18b8ec57d9d6a",
+    "url": "/cessini-mail/static/js/runtime-main.1ea3fd15.js"
+  }
+]);
